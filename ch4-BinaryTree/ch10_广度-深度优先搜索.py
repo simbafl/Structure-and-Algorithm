41a4d94610f68e6eb121ab93468486791609531d
fenglei110 <1652997150@qq.com>
2019-01-01 22:07:05 +0800

    add

---
+++ ch4-BinaryTree/ch10_广度-深度优先搜索.py
@@ -0,0 +1,27 @@
+"""以scrapy爬虫框架举例，默认为深度优先搜索"""
+
+def depth_tree(root):
+    """深度优先搜索，基于递归"""
+    if root is not None:
+        print(root.data)
+        if root._left is not None:
+            return depth_tree(root._left)
+        if root._right is not None:
+            return depth_tree(root._right)
+
+
+def level_tree(root):
+    """广度优先，基于队列"""
+    if root is None:
+        return 
+    my_queue = []
+    node = root
+    my_queue.append(node)
+    while my_queue:
+        node = my_queue.pop(0)
+        print(node.data)
+        if node.lchild is not None:
+            my_queue.append(node.lchild)
+        if node.rchild is not None:
+            my_queue.append(node.rchild)
+
